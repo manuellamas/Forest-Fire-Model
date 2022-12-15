655a85e48c9ee58c73d82779dc31c3fa35ae91c9
manuellamas <manuelbravolamas@gmail.com>
2022-12-15 11:22:13 +0000

    Initiating repo

---
+++ Code/main.py
@@ -0,0 +1,17 @@
+
+# Testing a Matplotlib animation
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+np.random.seed(42)
+data = np.random.random((50, 50, 50))
+
+fig, ax = plt.subplots()
+
+for i, img in enumerate(data):
+    ax.clear()
+    ax.imshow(img)
+    ax.set_title(f"frame {i}")
+    # Note that using time.sleep does *not* work here!
+    plt.pause(0.1)
---
+++ README.md
@@ -0,0 +1,13 @@
+
+- [ ] Make it show just without borders, without titles, just the animation
+
+
+Parameters:
+- [ ]  The p probability for lightining and tree spawning
+- [ ]  w,h number of "pixels" (i.e. squares)
+
+
+Colors/states
+- [ ] Red right after being burned
+- [ ] Green if currently a tree unburned
+- [ ] Black if nothing or after being burned
