a090211a30c832ba3851f2ce27d6811c6525914f
manuellamas <manuelbravolamas@gmail.com>
2022-12-27 22:43:18 +0000

    First complete working version of the Forest-Fire Model

--- Code/main_animation.py
+++ Code/main_animation.py
@@ -5,15 +5,105 @@ import numpy as np
 import random
 import copy # For list deepcopy
 
+import time # Measuring run rime
+
+
+"""
+Coordinates are Matrix like
+
+Point (1,0) is line 1 and column 0
+
+__
+P_
+
+X coordinate is vertical, from top to bottom
+Y coordinate is horizontal, from left to right
+
+"""
+
 
 # Setting seed
 # random.seed(42)
 
 
 def random_cell(w,h):
-    return random.randint(0, w - 1), random.randint(0, h - 1)
+    """ Returns coordinates of random cell, given the map limits """
+    return random.randint(0, h - 1), random.randint(0, w - 1)
 
 
+def get_neighbors(cell_map, cell, w, h):
+    """ Returns list of given cell neighbors' coordinates """
+    neighbors = []
+
+    x, y = cell
+
+
+    # Going by vertical borders, and within those horizontal borders
+
+    if cell[0] == 0: # TOP
+        if cell[1] == 0: # LEFT
+            neighbors.append((x+1, y)) # Below
+            neighbors.append((x+1, y+1)) # Below Right
+            neighbors.append((x, y+1)) # Right
+
+        elif cell[1] == w - 1: # RIGHT
+            neighbors.append((x+1, y)) # Below
+            neighbors.append((x+1, y-1)) # Below Left
+            neighbors.append((x, y-1)) # Left
+        
+        else:
+            neighbors.append((x+1, y-1)) # Below Left
+            neighbors.append((x+1, y)) # Below
+            neighbors.append((x+1, y+1)) # Below Right
+            neighbors.append((x, y-1)) # Left
+            neighbors.append((x, y+1)) # Right
+
+
+    elif cell[0] == h - 1: # BOT
+        if cell[1] == 0: # LEFT
+            neighbors.append((x-1, y)) # Above
+            neighbors.append((x-1, y+1)) # Above Right
+            neighbors.append((x, y+1)) # Right
+
+        elif cell[1] == w - 1: # RIGHT
+            neighbors.append((x-1, y)) # Above
+            neighbors.append((x-1, y-1)) # Above Left
+            neighbors.append((x, y-1)) # Left
+        
+        else:
+            neighbors.append((x-1, y-1)) # Above Left
+            neighbors.append((x-1, y)) # Above
+            neighbors.append((x-1, y+1)) # Above Right
+            neighbors.append((x, y-1)) # Left
+            neighbors.append((x, y+1)) # Right
+    
+    else:
+        if cell[1] == 0: # LEFT
+            neighbors.append((x+1, y)) # Below
+            neighbors.append((x+1, y+1)) # Below Right
+            neighbors.append((x, y+1)) # Right
+            neighbors.append((x-1, y+1)) # Above Right
+            neighbors.append((x-1, y)) # Above
+
+        elif cell[1] == w - 1: # RIGHT
+            neighbors.append((x-1, y)) # Above
+            neighbors.append((x-1, y-1)) # Above Left
+            neighbors.append((x, y-1)) # Left
+            neighbors.append((x+1, y-1)) # Below Left
+            neighbors.append((x+1, y)) # Below
+        
+        else:
+            neighbors.append((x-1, y+1)) # Above Right
+            neighbors.append((x-1, y)) # Above
+            neighbors.append((x-1, y-1)) # Above Left
+            neighbors.append((x, y-1)) # Left
+            neighbors.append((x+1, y-1)) # Below Left
+            neighbors.append((x+1, y)) # Below
+            neighbors.append((x+1, y+1)) # Below Right
+            neighbors.append((x, y+1)) # Right
+    
+    
+    return neighbors
 
 
 #################
@@ -24,8 +114,8 @@ def forestFireModel(g):
     tree_spawning_p = g
     lightnining_p = 1 - tree_spawning_p
 
-    num_iterations = 500
-    WIDTH = 50
+    num_iterations = 3_000
+    WIDTH = 75
     HEIGHT = 50
 
     # Colors for the different states
@@ -37,7 +127,7 @@ def forestFireModel(g):
 
     # cell_map_empty = [[[0, 0, 0] for j in range(HEIGHT)] for i in range(WIDTH)]
     # cell_map_empty = np.array([[[0, 0, 0] for j in range(HEIGHT)] for i in range(WIDTH)]) # Numpy arrays allow for reading values as list1[0,1,2] instead of list1[0][1][2]
-    cell_map_empty = np.array([[EMPTY for j in range(HEIGHT)] for i in range(WIDTH)]) # Numpy arrays allow for reading values as list1[0,1,2] instead of list1[0][1][2]
+    cell_map_empty = np.array([[EMPTY for j in range(WIDTH)] for i in range(HEIGHT)]) # Numpy arrays allow for reading values as list1[0,1,2] instead of list1[0][1][2]
     
     # Instead of [0,0,0] create a class for cell, and have two things, state, and color (This would require two "runs" one to change the state, and a second to change color)
     # Ooooor create a function that reads color (rgb triplet, according to the colors I've chosen) and tells us the state (Empty or Forest)
@@ -56,21 +146,30 @@ def forestFireModel(g):
     burning_trees = []
 
 
-    def update_plot(n, cell_map, teatet):
+    def update_plot(n, cell_map, burning_trees):
         ax.clear() # Clear/reset image
 
-        print(n, cell_map[1])
         old_cell_map = copy.deepcopy(cell_map) # A new one needs to be created so that the changes are made all at the same time
 
-        print(cell_map)
 
         # Randomly chosen cell
         chosen_cell = random_cell(WIDTH, HEIGHT)
 
-        for tree in burning_trees:
+        num_burn_trees = len(burning_trees)
+
+        for i in range(num_burn_trees):
+            tree = burning_trees.pop()
+
             ## Burn neighbors
-            
+
             # Get list of neighbors
+            neighbors = get_neighbors(cell_map, tree, WIDTH, HEIGHT)
+            
+            for neighbor in neighbors: # Neighbors of burning trees burn
+                if (old_cell_map[tuple(neighbor)] == TREE).all():
+                    cell_map[tuple(neighbor)] = BURN
+                    burning_trees.insert(0, tuple(neighbor)) # Push new tree at the beginning of the list (queue)
+
 
             ## Burning trees stop burning (and can in the same period become trees again)
             cell_map[tuple(tree)] = EMPTY # tuple allows for being read as input in a numpy array
@@ -80,38 +179,44 @@ def forestFireModel(g):
         if (old_cell_map[chosen_cell] == TREE).all(): # .all() checks if both arrays are equal
             random_num = random.random()
 
-            if random_num >= lightnining_p: # Lightning strikes and tree burns
+            if random_num <= lightnining_p: # Lightning strikes and tree burns
                 cell_map[chosen_cell] = BURN
-                burning_trees.append([chosen_cell])
-            
+                burning_trees.insert(0, chosen_cell) # Push new tree at the beginning of the list (queue)
 
         # If site is empty
         elif (old_cell_map[chosen_cell] == EMPTY).all(): # .all() checks if both arrays are equal
             random_num = random.random()
 
-            if random_num >= tree_spawning_p: # Tree spanws
+            if random_num <= tree_spawning_p: # Tree spanws
                 cell_map[chosen_cell] = TREE
 
-        else:
+        else: # No other state because if it was burning it no longer is, due to the for cycle above
             pass
 
-        # img = data[n]
+
         img = cell_map
 
         # https://matplotlib.org/stable/api/_as_gen/matplotlib.axes.Axes.imshow.html#matplotlib.axes.Axes.imshow
         ax.imshow(img)
-        print(cell_map)
 
         ax.set_title(f"frame {n}")
-        cell_map[1] += 1
-        return cell_map
+
+        return cell_map, burning_trees
 
 
-    ani = animation.FuncAnimation(fig, update_plot, num_iterations, fargs = (cell_map, 0), interval = 150, repeat = False) # Interval defines the speed of the animation (smaller value means a smaller duration and thus faster) and it works regardless the number of frames (iterations)
-    # ani = animation.FuncAnimation(fig, update_plot, num_iterations, fargs = (cell_map), interval = 150, repeat = False) # Interval defines the speed of the animation (smaller value means a smaller duration and thus faster) and it works regardless the number of frames (iterations)
+    ani = animation.FuncAnimation(fig, update_plot, num_iterations, fargs = (cell_map, burning_trees), interval = 1, repeat = False) # Interval defines the delay between frames in miliseconds (smaller value means a smaller duration and thus faster)
+    # ani = animation.FuncAnimation(fig, update_plot, num_iterations, fargs = (cell_map, burning_trees), interval = 300, repeat = False) # Interval defines the delay between frames in miliseconds (smaller value means a smaller duration and thus faster)
 
+    ani.save("Forest_fire_Model.mp4") # Make sure to create the folder before running
     # ani.save("Plot_animations\\Forest_fire_Model" + "" + ".mp4") # Make sure to create the folder before running
-    plt.show()
+    # plt.show()
 
 
-forestFireModel(0.5)
+
+
+start = time.time()
+
+forestFireModel(0.95)
+
+print("Running Time")
+print(time.time() - start)
